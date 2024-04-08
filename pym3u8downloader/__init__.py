b3edfcfd7c7c7d3d763abea5a0442c5c75c3d53d
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2024-04-08 20:20:33 +0100

    Introduced Linting (#2)

--- pym3u8downloader/__init__.py
+++ pym3u8downloader/__init__.py
@@ -28,11 +28,11 @@ __all__ = [
 ]
 __author__ = "coldsofttech"
 __description__ = """
-M3U8 Downloader is a Python class designed to download and concatenate video files from M3U8 playlists. 
-This class provides functionality to handle M3U8 playlist files, download video segments, concatenate them 
+M3U8 Downloader is a Python class designed to download and concatenate video files from M3U8 playlists.
+This class provides functionality to handle M3U8 playlist files, download video segments, concatenate them
 into a single video file, and manage various error conditions.
 """
 __name__ = "pym3u8downloader"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from pym3u8downloader.__main__ import M3U8Downloader, M3U8DownloaderError
